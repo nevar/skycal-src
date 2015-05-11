052e97b6c76b82b8a0467290449514b6876e2118
Slava Yurin <YurinVV@ya.ru>
2015-05-11 10:00:49 +0600

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,12 @@
 console.log('This would be the main JS file.');
 
 sigma.parsers.json(
-  'static/main_atlas.json', {container: 'sigma-container'},
+  'static/main_atlas.json',
+    { container: 'sigma-container'
+    , settings:
+      { defaultNodeColor: '#ffffff'
+      }
+    },
   function(s) {
   }
 );
