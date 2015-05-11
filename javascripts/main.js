10687a3d90a6b6e02388faf6b0bbc9d734226686
nevar <YurinVV@ya.ru>
2015-05-11 11:44:15 +0600

    Size

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,6 +11,7 @@ sigma.parsers.json(
 	'static/main_atlas.json',
 	{ renderer:
 		{ container: document.getElementById('sigma-container')
+		, type: 'canvas'
 		}
 	, settings:
 		{ defaultNodeColor: '#ffffff'
