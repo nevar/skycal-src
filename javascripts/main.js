2340634969a6530937f5da0944f9c25237139b38
nevar <YurinVV@ya.ru>
2015-05-11 10:30:20 +0600

    space to tab

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,12 @@
-console.log('This would be the main JS file.');
-
 sigma.parsers.json(
-  'static/main_atlas.json',
-    { container: 'sigma-container'
-    , settings:
-      { defaultNodeColor: '#ffffff'
-      }
-    },
-  function(s) {
-    CustomShapes.init(s);
-	s.refresh();
-  }
+	'static/main_atlas.json',
+	{ container: 'sigma-container'
+	, settings:
+		{ defaultNodeColor: '#ffffff'
+		}
+	},
+	function(s) {
+		CustomShapes.init(s);
+		s.refresh();
+	}
 );
