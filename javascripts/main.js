cdc9c5c65ca5b625ba43375385c97068d1f3096a
nevar <YurinVV@ya.ru>
2015-05-11 11:05:27 +0600

    fix image

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,34 +1,24 @@
 var images_url;
 var loaded = 0;
 
+sigma.utils.pkg('sigma.canvas.nodes');
+
 images_url =
 	[ "images/node/green.png"
 	];
 
-function init_graph() {
-	sigma.parsers.json(
-		'static/main_atlas.json',
-		{ renderer:
-			{ container: document.getElementById('sigma-container')
-			, type: 'canvas'
-			}
-		, settings:
-			{ defaultNodeColor: '#ffffff'
-			}
-		},
-		function(s) {
-			CustomShapes.init(s);
-			s.refresh();
+sigma.parsers.json(
+	'static/main_atlas.json',
+	{ renderer:
+		{ container: document.getElementById('sigma-container')
+		, type: 'canvas'
 		}
-	);
-}
-
-for (var i = images_url.length; i--; ) {
-	sigma.canvas.nodes.image.cache(
-		images_url[i],
-		function() {
-			if (++loaded === images_url.length) {
-				init_graph();
-			}
-		});
-}
+	, settings:
+		{ defaultNodeColor: '#ffffff'
+		}
+	},
+	function(s) {
+		CustomShapes.init(s);
+		s.refresh();
+	}
+);
